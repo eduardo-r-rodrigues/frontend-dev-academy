b828b1442e201e85986c1ec92201a8043162fc98
Daniel <dani.araujo2002@gmail.com>
2025-04-28 15:51:04 -0300

    feat(component): gerar ExecuteComponent para rodar prompt na IA

---
+++ src/app/components/execute/execute.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-execute',
+  imports: [],
+  templateUrl: './execute.component.html',
+  styleUrl: './execute.component.scss'
+})
+export class ExecuteComponent {
+
+}
