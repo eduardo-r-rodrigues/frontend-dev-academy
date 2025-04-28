ab5c5a0776c79819b797874efc912d7cacb8bc23
Daniel <dani.araujo2002@gmail.com>
2025-04-28 15:50:57 -0300

    feat(component): gerar PromptFormComponent para criar/editar prompt

---
+++ src/app/components/prompt-form/prompt-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prompt-form',
+  imports: [],
+  templateUrl: './prompt-form.component.html',
+  styleUrl: './prompt-form.component.scss'
+})
+export class PromptFormComponent {
+
+}
