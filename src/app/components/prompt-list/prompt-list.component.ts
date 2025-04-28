e1dae2219799d077618aef61de1613c04998a3f0
Daniel <dani.araujo2002@gmail.com>
2025-04-28 15:50:50 -0300

    feat(component): gerar PromptListComponent para listar prompts

---
+++ src/app/components/prompt-list/prompt-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prompt-list',
+  imports: [],
+  templateUrl: './prompt-list.component.html',
+  styleUrl: './prompt-list.component.scss'
+})
+export class PromptListComponent {
+
+}
